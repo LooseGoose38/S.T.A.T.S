991061204a3477913ab36167697c2a49dc402ed2
LooseGoose38 <cendejas.2@wright.edu>
2026-09-05 01:45:45 -0400

    Quick changes

--- utils/scraper.js
+++ utils/scraper.js
@@ -49,7 +49,13 @@ async function scrapeTrophyGuide(targetUrl, targetTrophy) {
 
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu'
+        ]
     });
 
     const page = await browser.newPage();
